fd0e61d88f3c9b96745e8d7e69bdfb27ec2c6123
gupta-suryansh <suryansh30052005@gmail.com>
2026-09-06 23:11:57 +0530

    respond to multiple pings

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.*;
+import java.util.*;
 
 
 public class Main{
@@ -13,14 +13,21 @@ public class Main{
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept();
 
+            InputStream inputStream = clientSocket.getInputStream();
             OutputStream outputStream = clientSocket.getOutputStream();
-            outputStream.write("+PONG\r\n".getBytes());
+            Scanner sc = new Scanner(inputStream);
+            while(sc.hasNextLine()){
+                String nextLine = sc.nextLine();
+                if(nextLine.contains("PING")){
+                    outputStream.write("+PONG\r\n".getBytes());
+                }
+            } 
         }catch(IOException e){
             System.out.println("IOException: "+e.getMessage());
         }finally{
             try{
                 if(clientSocket!=null){
-                    clientSocket.close();
+                    clientSocket.close();  //in case of erros or manually closing 
                 }
             }catch(IOException e){
                 System.out.println("IOException: "+e.getMessage());
