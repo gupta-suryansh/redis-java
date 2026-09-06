d43c6097c058e066fa7d87479dc4bafaf2451ff0
gupta-suryansh <suryansh30052005@gmail.com>
2026-09-06 22:49:50 +0530

    respond to ping

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.*;
+
+
+public class Main{
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 6379;
+        try{
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            clientSocket = serverSocket.accept();
+
+            OutputStream outputStream = clientSocket.getOutputStream();
+            outputStream.write("+PONG\r\n".getBytes());
+        }catch(IOException e){
+            System.out.println("IOException: "+e.getMessage());
+        }finally{
+            try{
+                if(clientSocket!=null){
+                    clientSocket.close();
+                }
+            }catch(IOException e){
+                System.out.println("IOException: "+e.getMessage());
+            }
+        }
+    }
+}
